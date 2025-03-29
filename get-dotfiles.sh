c88008f6e115ce1299bcb14a20f8769fd12c33ba
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-03-29 12:23:17 -0300

    add: .aliases, lazygit, .gitconfig and .zshrc

--- get-dotfiles.sh
+++ get-dotfiles.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cp ~/.gitconfig ./
+cp ~/.aliases.sh ./
+cp ~/.zshrc ./
 cp -r ~/.config/nvim ./.config/
+cp -r ~/.config/lazygit/ ./.config
--- set-dotfiles.sh
+++ set-dotfiles.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+cp ./.gitconfig ~/.gitconfig
+
+cp ./.aliases.sh ~/.aliases.sh
+cp ./.zshrc ~/.zshrc
+
 rm -rf ~/.config/nvim
 cp -r ./.config/nvim/ ~/.config/nvim
+
+rm -rf ~/.config/lazygit/
+cp -r ./.config/lazygit/ ~/.config/lazygit/
