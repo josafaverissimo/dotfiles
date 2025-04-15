91551f1f8278979c140870255768c6246d03ef21
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-04-15 00:47:15 -0300

    hypr configs

---
+++ .config/hypr/scripts/toggle_opacity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_opacity=$(hyprctl getoption decoration:inactive_opacity | rg --pcre2 "(?<=float:\s)\d\.\d" -o)
+
+opaque=1.0
+active_translucent=0.9
+inactive_translucent=0.8
+
+echo $current_opacity
+echo $opaque
+
+if [[ "$current_opacity" == "$opaque" ]]; then
+  hyprctl keyword decoration:inactive_opacity "$inactive_translucent"
+  hyprctl keyword decoration:active_opacity "$active_translucent"
+else
+  hyprctl keyword decoration:inactive_opacity "$opaque"
+  hyprctl keyword decoration:active_opacity "$opaque"
+fi
