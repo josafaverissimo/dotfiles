d961bf01e274f0db7214b550a01f961a85bd475a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-04-29 17:17:28 -0300

    add: smear cursor

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -4,6 +4,17 @@ return {
   "nvim-lualine/lualine.nvim",
   "andweeb/presence.nvim",
   {
+    "sphamba/smear-cursor.nvim",
+    opts = { -- Default  Range
+      legacy_computing_symbols_support = true
+      -- stiffness = 0.8, -- 0.6      [0, 1]
+      -- trailing_stiffness = 0.5, -- 0.4      [0, 1]
+      -- stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      -- trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      -- distance_stop_animating = 0.5, -- 0.1      > 0
+    },
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
