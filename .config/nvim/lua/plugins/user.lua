73a6e7b49b2f7d19cca2540667006df34e9c5fae
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-05-06 22:47:37 -0300

    feat(kitty): set kitty base conf

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -4,16 +4,6 @@ return {
   "nvim-lualine/lualine.nvim",
   "andweeb/presence.nvim",
   {
-    "kylechui/nvim-surround",
-    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
