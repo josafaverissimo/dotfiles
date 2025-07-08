9bbcabeb9e0518028a3c97f00d70481a247dec40
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-07-08 17:11:50 -0300

    feat: telescope find hind files

--- .config/nvchad/lua/configs/telescope-setup.lua
+++ .config/nvchad/lua/configs/telescope-setup.lua
@@ -1,9 +1,15 @@
 local map = vim.keymap.set
 local builtin = require "telescope.builtin"
-local actions = require("telescope.actions")
+local actions = require "telescope.actions"
 
 map("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-map("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+map("n", "<leader>fF", function()
+  builtin.find_files { hidden = true, no_ignore = true }
+end, { desc = "Telescope find hidden files" })
+map("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
+map("n", "<leader>fW", function()
+  builtin.live_grep { additional_args = { "--hidden", "--no-ignore" } }
+end, { desc = "Telescope live grep hidden files" })
 map("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 map("n", "<leader>fg", builtin.git_status, { desc = "Telescope git status" })
@@ -11,13 +17,12 @@ map("n", "<leader>fc", builtin.git_commits, { desc = "Telescope git commits" })
 map("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope lsp references" })
 map("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
 
-require("telescope").setup{
+require("telescope").setup {
   defaults = {
     mappings = {
       i = {
         ["<C-u>"] = false,
       },
     },
-  }
+  },
 }
-
